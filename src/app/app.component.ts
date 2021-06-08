7e4fbc05869c65faca81aa66f2bb1fb4d460293e
Vinna <69981687+vig37@users.noreply.github.com>
2021-06-08 13:55:34 -0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: ['./app.component.css']
 })
 export class AppComponent {}
